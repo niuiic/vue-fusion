4191a1e14cafc53c8b7692679061786caaefc13e
niuiic <niuiicde@gmail.com>
2024-04-17 16:15:13 +0800

    chore: update commit types

--- .commitlintrc.cjs
+++ .commitlintrc.cjs
@@ -1,11 +1,7 @@
 module.exports = {
   extends: ['@commitlint/config-conventional'],
   rules: {
-    'type-enum': [
-      2,
-      'always',
-      ['sync', 'merge', 'chore', 'ci', 'docs', 'feat', 'fix', 'perf', 'refactor', 'revert', 'style', 'test']
-    ]
+    'type-enum': [2, 'always', ['chore', 'docs', 'feat', 'fix', 'refactor', 'test']]
   },
   prompt: {
     questions: {
@@ -17,11 +13,6 @@ module.exports = {
             title: 'Features',
             emoji: '✨'
           },
-          merge: {
-            description: '合并分支',
-            title: 'Merge Branches',
-            emoji: '☄'
-          },
           fix: {
             description: '修复bug',
             title: 'Bug Fixes',
@@ -32,45 +23,20 @@ module.exports = {
             title: 'Documentation',
             emoji: '📚'
           },
-          style: {
-            description: '修改代码样式但不影响代码意义，如格式化代码',
-            title: 'Styles',
-            emoji: '💎'
-          },
           refactor: {
             description: '代码重构，不添加新功能，也不修复bug',
             title: 'Code Refactoring',
             emoji: '📦'
-          },
-          perf: {
-            description: '提高程序性能或优化用户体验（如优化界面样式）',
-            title: 'Performance Improvements',
-            emoji: '🚀'
           },
           test: {
             description: '测试相关',
             title: 'Tests',
             emoji: '🚨'
           },
-          sync: {
-            description: '同步主线或分支的bug',
-            title: 'Sync',
-            emoji: '⚙️'
-          },
-          ci: {
-            description: '持续集成相关',
-            title: 'Continuous Integrations',
-            emoji: '🛠'
-          },
           chore: {
             description: '与src无关的变动，如构建、工程依赖、工具',
             title: 'Chores',
             emoji: '♻️'
-          },
-          revert: {
-            description: '回滚到之前的版本',
-            title: 'Reverts',
-            emoji: '🗑'
           }
         }
       },
