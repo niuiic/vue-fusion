2af7939f3d12fedfeb2b39db2343615bb096db0c
niuiic <niuiicde@gmail.com>
2024-06-14 10:01:13 +0800

    refactor: builtins

--- packages/builtins/src/enum.ts
+++ packages/builtins/src/enum.ts
@@ -3,7 +3,7 @@ export type Enum<K extends string | symbol | number, V> = Record<K, V>
 export function useEnum<K extends string | symbol | number, V>(map: Enum<K, V>, defaultKey: K, defaultValue: V) {
   const toValue = (key: K): V => {
     if (Reflect.has(map, key)) {
-      return Reflect.get(map, key)
+      return map[key]
     }
     return defaultValue
   }
