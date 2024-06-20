b29da4139afa298fc443e3405ee39816b48c3cff
niuiic <niuiicde@gmail.com>
2024-06-20 17:00:09 +0800

    feat(builtins): concurrent

---
+++ packages/builtins/src/concurrent/README.md
@@ -0,0 +1,13 @@
+# concurrent
+
+## 用法
+
+```typescript
+const queryXApi = () => Promise.resolve(1)
+const queryYApi = () => Promise.reject(1)
+const queryZApi = () => Promise.resolve(1)
+
+// 并发发送请求，并限制最多同时执行两个，防止后端卡顿
+// queryYApi被rejected，则concurrent会throw error
+const [result1, result2, result3] = await concurrent([queryXApi, queryYApi, queryZApi], 2)
+```
---
+++ packages/builtins/src/concurrent/index.ts
@@ -0,0 +1,22 @@
+type Fn<T> = () => T | Promise<T>
+
+export const concurrent = async <T>(fns: Fn<T>[], limit: number): Promise<T[]> => {
+  const result: T[] = []
+
+  const execFn = async (fn: Fn<T>, i: number) => {
+    try {
+      const res = await fn()
+      result[i] = res
+    } finally {
+      if (index < fns.length) {
+        index = index + 1
+        await execFn(fns[index - 1], index - 1)
+      }
+    }
+  }
+
+  let index = Math.min(limit, fns.length)
+  await Promise.all(fns.slice(0, index).map((fn, i) => execFn(fn, i)))
+
+  return result
+}
