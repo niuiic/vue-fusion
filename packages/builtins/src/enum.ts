a9056b846989ba76b3a33eb9a85c38c6c81c2839
niuiic <niuiicde@gmail.com>
2024-08-13 09:11:57 +0800

    feat: useEnum

--- packages/builtins/src/enum.ts
+++ packages/builtins/src/enum.ts
@@ -22,5 +22,15 @@ export function useEnum<K extends string | symbol | number, V>(map: Enum<K, V>, 
     return fixedKeys.includes(value)
   }
 
-  return { toValue, toKey, map, hasKey }
+  function mapObj(): Enum<K, V>
+  function mapObj<Keys extends K[]>(keys: Keys): Enum<Keys[number], V>
+  function mapObj<Keys extends K[]>(keys?: Keys) {
+    if (!keys) {
+      return map
+    }
+
+    return Object.fromEntries(keys.map((key) => [key, toValue(key)])) as Enum<Keys[number], V>
+  }
+
+  return { toValue, toKey, mapObj, hasKey }
 }
