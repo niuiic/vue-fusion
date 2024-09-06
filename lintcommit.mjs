a38e2dceb3d55813737cd31c604d2bdd7f370516
niuiic <niuiicde@gmail.com>
2024-09-06 20:24:31 +0800

    chore: commitlint

---
+++ lintcommit.mjs
@@ -0,0 +1,11 @@
+import { readFileSync } from 'fs'
+
+const logFile = process.argv[2]
+const msg = readFileSync(logFile).toString()
+
+const regex = /(feat|fix|docs|refactor|test|chore)(?:\((.+)\))?: (.*)/
+
+if (!regex.test(msg)) {
+  console.error(['git commit message does not conform to the established rules', regex.toString()].join('\n'))
+  process.exit(1)
+}
