c55a10a5a38f3e06cf53fa79fe60ca2b29aa49e2
niuiic <niuiicde@gmail.com>
2024-09-06 09:13:50 +0800

    refactor: useDynFn -> useAsyncFn

--- packages/builtins/src/useAsyncFn.ts
+++ packages/builtins/src/useAsyncFn.ts
@@ -1,6 +1,6 @@
 import type { AnyFunction } from './types'
 
-export const useDynFn =
+export const useAsyncFn =
   <T extends AnyFunction>(loadFn: () => Promise<T>): ((...args: Parameters<T>) => Promise<Awaited<ReturnType<T>>>) =>
   (...args: any[]) =>
     loadFn().then((fn) => fn(...args))
