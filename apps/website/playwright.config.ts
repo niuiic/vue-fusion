bebdb4b7b7128d8989aa6463dc8737bd0d0c2a45
niuiic <niuiicde@gmail.com>
2024-12-10 10:01:02 +0800

    chore: add playwright

---
+++ apps/website/playwright.config.ts
@@ -0,0 +1,22 @@
+import { defineConfig, devices } from '@playwright/test'
+
+export default defineConfig({
+  testDir: './src',
+  testMatch: /.*\.e2e\.ts/,
+  fullyParallel: true,
+  reporter: 'html',
+  use: {
+    trace: 'on-first-retry',
+    headless: false
+  },
+  projects: [
+    {
+      name: 'chromium',
+      use: { ...devices['Desktop Chrome'] }
+    }
+  ],
+  webServer: {
+    command: 'pnpm dev:mock',
+    url: 'http://127.0.0.1:9000'
+  }
+})
