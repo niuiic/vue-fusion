5825905abeeeb5b9e1766c9527854350094781a6
niuiic <niuiicde@gmail.com>
2024-12-11 11:36:03 +0800

    refactor: mock

--- apps/website/src/module/user/index.ts
+++ apps/website/src/module/user/index.ts
@@ -5,6 +5,8 @@ import { userModuleSymbols } from './symbol'
 
 export const userModule = new Container()
 
-userModule.bind(userModuleSymbols.getMockDAO).toConstantValue(() => import('./mock').then((x) => x.userDAOMock))
+userModule
+  .bind(userModuleSymbols.getMockDAO)
+  .toConstantValue(() => (__MOCK__ ? import('./mock').then((x) => x.userDAOMock) : Promise.resolve()))
 userModule.bind(userModuleSymbols.userDAO).to(UserDAO).inSingletonScope()
 userModule.bind(userModuleSymbols.userService).to(UserService).inTransientScope()
