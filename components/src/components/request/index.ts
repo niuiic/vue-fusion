34b69eaaccf9e0dfbe258cfd558ec392720eccf7
niuiic <niuiicde@gmail.com>
2025-01-23 17:09:26 +0800

    feat: request

--- components/src/components/request/index.ts
+++ components/src/components/request/index.ts
@@ -1,28 +1,99 @@
 import type { AnyObject } from '@/components/types'
 import type { AxiosInstance, AxiosRequestConfig, AxiosResponse, CreateAxiosDefaults } from 'axios'
 import axios from 'axios'
+import { isNil } from '../isNil'
+
+const defaultCacheLifetime = 1000
+
+interface CacheOptions {
+  key: string
+  lifetime?: number
+}
 
 export class Request {
   private instance: AxiosInstance
+  private cache = new Map<
+    string,
+    {
+      expiry: number
+      promise: Promise<any>
+    }
+  >()
 
   public constructor(args: CreateAxiosDefaults) {
     this.instance = axios.create(args)
   }
 
-  public async get<R>(url: string, options?: AxiosRequestConfig): Promise<R> {
-    return this.instance.get(url, fixedOptions(options)).then(resolveResult)
+  public async get<R>(url: string, options?: AxiosRequestConfig, cacheOptions?: CacheOptions): Promise<R> {
+    const request = () => this.instance.get(url, fixedOptions(options)).then(resolveResult)
+    return this.cachedRequest(request, url, options, cacheOptions)
   }
 
-  public async post<R>(url: string, data?: AnyObject, options?: AxiosRequestConfig): Promise<R> {
-    return this.instance.post(url, data, fixedOptions(options)).then(resolveResult)
+  public async post<R>(
+    url: string,
+    data?: AnyObject,
+    options?: AxiosRequestConfig,
+    cacheOptions?: CacheOptions
+  ): Promise<R> {
+    const request = () => this.instance.post(url, data, fixedOptions(options)).then(resolveResult)
+    return this.cachedRequest(request, url, options, cacheOptions)
   }
 
-  public async rawGet<R>(url: string, options?: AxiosRequestConfig): Promise<AxiosResponse<R>> {
-    return this.instance.get(url, fixedOptions(options))
+  public async rawGet<R>(
+    url: string,
+    options?: AxiosRequestConfig,
+    cacheOptions?: CacheOptions
+  ): Promise<AxiosResponse<R>> {
+    const request = () => this.instance.get(url, fixedOptions(options))
+    return this.cachedRequest(request, url, options, cacheOptions)
   }
 
-  public async rawPost<R>(url: string, data?: AnyObject, options?: AxiosRequestConfig): Promise<AxiosResponse<R>> {
-    return this.instance.post(url, data, fixedOptions(options))
+  public async rawPost<R>(
+    url: string,
+    data?: AnyObject,
+    options?: AxiosRequestConfig,
+    cacheOptions?: CacheOptions
+  ): Promise<AxiosResponse<R>> {
+    const request = () => this.instance.post(url, data, fixedOptions(options))
+    return this.cachedRequest(request, url, options, cacheOptions)
+  }
+
+  private async cachedRequest<R>(
+    request: () => Promise<R>,
+    url: string,
+    options?: AxiosRequestConfig,
+    cacheOptions?: CacheOptions
+  ) {
+    if (isNil(cacheOptions?.key)) {
+      return request()
+    }
+
+    const cacheKey = getCacheKey(url, cacheOptions.key, options?.baseURL)
+    const cachedPromise = this.getCachedPromise(cacheKey)
+    if (!cachedPromise) {
+      const promise = request()
+      this.cache.set(cacheKey, {
+        expiry: new Date().getTime() + (cacheOptions.lifetime ?? defaultCacheLifetime),
+        promise
+      })
+      return promise
+    }
+
+    return cachedPromise
+  }
+
+  private getCachedPromise(cacheKey: string) {
+    const cacheValue = this.cache.get(cacheKey)
+    if (!cacheValue) {
+      return
+    }
+
+    if (cacheValue.expiry > new Date().getTime()) {
+      this.cache.delete(cacheKey)
+      return
+    }
+
+    return cacheValue.promise
   }
 }
 
@@ -47,3 +118,5 @@ const resolveResult = (response?: AxiosResponse) => {
   }
   throw new Error(response?.data?.code)
 }
+
+const getCacheKey = (url: string, key: string, baseURL?: string) => `${baseURL ?? '-'}:${url}:${key}`
