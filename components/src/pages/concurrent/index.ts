9c61e7b1215bb42e622142504fde7dd8409907f4
niuiic <niuiicde@gmail.com>
2025-02-10 13:58:17 +0800

    docs: concurrent

---
+++ components/src/pages/concurrent/index.ts
@@ -0,0 +1,25 @@
+import doc from '@/components/concurrent/README.md?raw'
+import source from '@/components/concurrent/index.ts?raw'
+import { CompStatus, type Page } from '@/page'
+
+export default {
+  name: 'concurrent',
+  status: [CompStatus.Approved],
+  author: 'nsc',
+  auditor: 'nsc',
+  category: 'api',
+  tags: ['desktop', 'mobile'],
+  docs: [
+    {
+      label: '文档',
+      language: 'markdown',
+      code: doc
+    },
+    {
+      label: '源码',
+      language: 'typescript',
+      code: source
+    }
+  ],
+  desc: '并发任务并限制最大并发数。'
+} satisfies Page
