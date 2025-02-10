e9d7f410a43f0d6fba7a4efe7ceeedb16003459b
niuiic <niuiicde@gmail.com>
2025-02-10 13:29:05 +0800

    docs: time

---
+++ components/src/pages/time/index.ts
@@ -0,0 +1,19 @@
+import source from '@/components/time.ts?raw'
+import { CompStatus, type Page } from '@/page'
+
+export default {
+  name: 'time',
+  status: [CompStatus.Approved],
+  author: 'nsc',
+  auditor: 'nsc',
+  category: 'api',
+  tags: ['desktop', 'mobile'],
+  docs: [
+    {
+      label: '源码',
+      language: 'typescript',
+      code: source
+    }
+  ],
+  desc: '格式化时间。'
+} satisfies Page
