5d583e117021af874161fcb52c887eb24c15b5de
niuiic <niuiicde@gmail.com>
2025-02-10 15:50:09 +0800

    docs: nextFrame

---
+++ components/src/components/nextFrame/README.md
@@ -0,0 +1,10 @@
+# nextFrame
+
+用于优化性能。
+
+```typescript
+// 等待一帧。
+await nextFrame()
+// 下面的任务将在下一帧执行。
+foo()
+```
---
+++ components/src/pages/nextFrame/index.ts
@@ -0,0 +1,25 @@
+import doc from '@/components/nextFrame/README.md?raw'
+import source from '@/components/nextFrame/index.ts?raw'
+import { CompStatus, type Page } from '@/page'
+
+export default {
+  name: 'nextFrame',
+  status: [CompStatus.Approved],
+  author: 'nsc',
+  auditor: 'nsc',
+  category: 'api',
+  tags: ['desktop', 'mobile'],
+  docs: [
+    {
+      label: '文档',
+      language: 'markdown',
+      code: doc
+    },
+    {
+      label: '源码',
+      language: 'typescript',
+      code: source
+    }
+  ],
+  desc: '等待下一帧再执行任务。'
+} as Page
