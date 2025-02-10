cd45a21d0c80ed4d4a961791e6473a2e7cec86ae
niuiic <niuiicde@gmail.com>
2025-02-10 15:45:20 +0800

    docs: result

--- components/src/components/result.ts
+++ components/src/components/result.ts
@@ -1,4 +1,4 @@
-export enum Result {
+export const enum Result {
   Done,
   Cancel,
   Error
---
+++ components/src/pages/result/index.ts
@@ -0,0 +1,19 @@
+import source from '@/components/result.ts?raw'
+import { CompStatus, type Page } from '@/page'
+
+export default {
+  name: 'result',
+  status: [CompStatus.Approved],
+  author: 'nsc',
+  auditor: 'nsc',
+  category: 'api',
+  tags: ['desktop', 'mobile'],
+  docs: [
+    {
+      label: '源码',
+      language: 'typescript',
+      code: source
+    }
+  ],
+  desc: '标准结果状态枚举。'
+} satisfies Page
